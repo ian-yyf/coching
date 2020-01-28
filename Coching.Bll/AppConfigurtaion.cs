cdd5650ac182f45df960653cce962f1074baea4c
yyf_book <yyf_book@yfhl.com>
2020-01-28 17:02:37 +0800

    配置

---
+++ Coching.Bll/AppConfigurtaion.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coching.Bll
+{
+    class AppConfigurtaion
+    {
+        public static IConfiguration Configuration { get; set; }
+        static AppConfigurtaion()
+        {
+            var path = "thirdaccounts.release.json";
+            Configuration = new ConfigurationBuilder()
+            .Add(new JsonConfigurationSource { Path = path, ReloadOnChange = true })
+            .Build();
+        }
+    }
+}
--- Coching.Bll/ThirdAccounts.cs
+++ Coching.Bll/ThirdAccounts.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,364 +7,50 @@ namespace Coching.Bll
 {
     public static class ThirdAccounts
     {
-        public static String AsApiAppId
-        {
-            get
-            {
-                return "Cuscom-As-Api";
-            }
-        }
-
-        public static String AsApiAppSecret
-        {
-            get
-            {
-                return "DAKACHUANMEN-AS-API-201906031247";
-            }
-        }
-
-        public static String AuthorizerThirdLoginUrl
-        {
-            get
-            {
-                return "https://api.darkast.com/Login";
-            }
-        }
-
-        public static String AuthorizerThirdLoginAppId
-        {
-            get
-            {
-                return "Cuscom-Login";
-            }
-        }
-
-        public static String AuthorizerThirdLoginAppSecret
-        {
-            get
-            {
-                return "YUNIKEFU-LOGIN-201912162255-0001";
-            }
-        }
-
-        public static String ThirdLoginUrl
-        {
-            get
-            {
-                return "https://api.darkast.com/Login";
-            }
-        }
-
-        public static String ThirdLoginAppId
-        {
-            get
-            {
-                return "DaKa-Login";
-            }
-        }
-
-        public static String ThirdLoginAppSecret
-        {
-            get
-            {
-                return "Daka-Chuanmei-Login-20190730-001";
-            }
-        }
-
-        public static String CaptchaUrl
-        {
-            get
-            {
-                return "https://api.darkast.com/Captcha";
-            }
-        }
-
-        public static String CaptchaAppId
-        {
-            get
-            {
-                return "App002";
-            }
-        }
-
-        public static String CaptchaAppSecret
-        {
-            get
-            {
-                return "QWERTYUIOPASDFGHJKLZXCVBNM123456";
-            }
-        }
-
-        public static String ShortMessageUrl
-        {
-            get
-            {
-                return "https://api.darkast.com/ShortMessage";
-            }
-        }
-
-        public static String ShortMessageAppId
-        {
-            get
-            {
-                return "ShortMessage";
-            }
-        }
-
-        public static String ShortMessageAppSecret
-        {
-            get
-            {
-                return "TONGYONG-SHORTMESSAGE-SNHCY-0001";
-            }
-        }
-
-        public static String ShortMessageSign
-        {
-            get
-            {
-                return "考成法";
-            }
-        }
-
-        public static String ShortMessageTemplate
-        {
-            get
-            {
-                return "SMS_152175081";
-            }
-        }
-
-        public static String PayUrl
-        {
-            get
-            {
-                return "https://api.darkast.com/Pay/Pay";
-            }
-        }
-
-        public static String EnterprisePayUrl
-        {
-            get
-            {
-                return "https://api.darkast.com/Pay/EnterprisePay";
-            }
-        }
-
-        public static String PayAppId
-        {
-            get
-            {
-                return "DaKa-Pay";
-            }
-        }
-
-        public static String PayAppSecret
-        {
-            get
-            {
-                return "Daka-Chuanmei-Pay-20190730-00001";
-            }
-        }
-
-        public static String AuthorizerQrCodeUrl
-        {
-            get
-            {
-                return "https://api.darkast.com/QrCode/Create";
-            }
-        }
-
-        public static String AuthorizerQrCodeAppId
-        {
-            get
-            {
-                return "Cuscom-QrCode";
-            }
-        }
-
-        public static String AuthorizerQrCodeAppSecret
-        {
-            get
-            {
-                return "YUNIKEFU-QRCODE-201912162255-001";
-            }
-        }
-
-        public static String QrCodeUrl
-        {
-            get
-            {
-                return "https://api.darkast.com/QrCode/Create";
-            }
-        }
-
-        public static String QrCodeAppId
-        {
-            get
-            {
-                return "DaKa-QrCode";
-            }
-        }
-
-        public static String QrCodeAppSecret
-        {
-            get
-            {
-                return "Daka-Chuanmei-QrCode-20190730-01";
-            }
-        }
-
-        public static String AuthorizerWechatUrl
-        {
-            get
-            {
-                return "https://api.darkast.com/Wechat";
-            }
-        }
-
-        public static String AuthorizerWechatAppId
-        {
-            get
-            {
-                return "Cuscom-Wechat";
-            }
-        }
-
-        public static String AuthorizerWechatAppSecret
-        {
-            get
-            {
-                return "Wechat-Cuscom-20191118-SecretX01";
-            }
-        }
-
-        public static String WechatUrl
-        {
-            get
-            {
-                return "https://api.darkast.com/Wechat";
-            }
-        }
-
-        public static String WechatAppId
-        {
-            get
-            {
-                return "DaKa-Wechat";
-            }
-        }
-
-        public static String WechatAppSecret
-        {
-            get
-            {
-                return "Daka-Chuanmei-Wechat-20190730-01";
-            }
-        }
-
-        public static String EventAppId
-        {
-            get
-            {
-                return "DaKa-Event";
-            }
-        }
-
-        public static String EventAppSecret
-        {
-            get
-            {
-                return "Daka-Chuanmei-Event-20190730-001";
-            }
-        }
-
-        public static String PlatformEventAppId
-        {
-            get
-            {
-                return "Cuscom-Event";
-            }
-        }
-
-        public static String PlatformEventAppSecret
-        {
-            get
-            {
-                return "Event-Cuscom-20191118000-SecretX";
-            }
-        }
-
-        public static String FileUrl
-        {
-            get
-            {
-                return "https://api.darkast.com/File";
-            }
-        }
-
-        public static String FileAppId
-        {
-            get
-            {
-                return "CommonImages";
-            }
-        }
-
-        public static String FileAppSecret
-        {
-            get
-            {
-                return "COMMON-IMAGES-201905300827-00001";
-            }
-        }
-
-        public static String ReverseGeocodingUrl
-        {
-            get
-            {
-                return "https://api.darkast.com/Map/ReverseGeocoding";
-            }
-        }
-
-        public static String MapAppId
-        {
-            get
-            {
-                return "Map";
-            }
-        }
-
-        public static String MapAppSecret
-        {
-            get
-            {
-                return "MAP-APPSECRET-2019-10-19-20-53-Y";
-            }
-        }
-
-        public static String AuthorizeAppId
-        {
-            get
-            {
-                return "Cuscom-Platform";
-            }
-        }
-
-        public static String AuthorizeAppSecret
-        {
-            get
-            {
-                return "Platform-Cuscom-20191118-SecretX";
-            }
-        }
-
-        public static String AuthorizeUrl
-        {
-            get
-            {
-                return "https://api.darkast.com/Platform/Authorize";
-            }
-        }
+        public static String AsApiAppId => AppConfigurtaion.Configuration["AsApiAppId"];
+        public static String AsApiAppSecret => AppConfigurtaion.Configuration["AsApiAppSecret"];
+        public static String AuthorizerThirdLoginUrl => AppConfigurtaion.Configuration["AuthorizerThirdLoginUrl"];
+        public static String AuthorizerThirdLoginAppId => AppConfigurtaion.Configuration["AuthorizerThirdLoginAppId"];
+        public static String AuthorizerThirdLoginAppSecret => AppConfigurtaion.Configuration["AuthorizerThirdLoginAppSecret"];
+        public static String ThirdLoginUrl => AppConfigurtaion.Configuration["ThirdLoginUrl"];
+        public static String ThirdLoginAppId => AppConfigurtaion.Configuration["ThirdLoginAppId"];
+        public static String ThirdLoginAppSecret => AppConfigurtaion.Configuration["ThirdLoginAppSecret"];
+        public static String CaptchaUrl => AppConfigurtaion.Configuration["CaptchaUrl"];
+        public static String CaptchaAppId => AppConfigurtaion.Configuration["CaptchaAppId"];
+        public static String CaptchaAppSecret => AppConfigurtaion.Configuration["CaptchaAppSecret"];
+        public static String ShortMessageUrl => AppConfigurtaion.Configuration["ShortMessageUrl"];
+        public static String ShortMessageAppId => AppConfigurtaion.Configuration["ShortMessageAppId"];
+        public static String ShortMessageAppSecret => AppConfigurtaion.Configuration["ShortMessageAppSecret"];
+        public static String ShortMessageSign => AppConfigurtaion.Configuration["ShortMessageSign"];
+        public static String ShortMessageTemplate => AppConfigurtaion.Configuration["ShortMessageTemplate"];
+        public static String PayUrl => AppConfigurtaion.Configuration["PayUrl"];
+        public static String EnterprisePayUrl => AppConfigurtaion.Configuration["EnterprisePayUrl"];
+        public static String PayAppId => AppConfigurtaion.Configuration["PayAppId"];
+        public static String PayAppSecret => AppConfigurtaion.Configuration["PayAppSecret"];
+        public static String AuthorizerQrCodeUrl => AppConfigurtaion.Configuration["AuthorizerQrCodeUrl"];
+        public static String AuthorizerQrCodeAppId => AppConfigurtaion.Configuration["AuthorizerQrCodeAppId"];
+        public static String AuthorizerQrCodeAppSecret => AppConfigurtaion.Configuration["AuthorizerQrCodeAppSecret"];
+        public static String QrCodeUrl => AppConfigurtaion.Configuration["QrCodeUrl"];
+        public static String QrCodeAppId => AppConfigurtaion.Configuration["QrCodeAppId"];
+        public static String QrCodeAppSecret => AppConfigurtaion.Configuration["QrCodeAppSecret"];
+        public static String AuthorizerWechatUrl => AppConfigurtaion.Configuration["AuthorizerWechatUrl"];
+        public static String AuthorizerWechatAppId => AppConfigurtaion.Configuration["AuthorizerWechatAppId"];
+        public static String AuthorizerWechatAppSecret => AppConfigurtaion.Configuration["AuthorizerWechatAppSecret"];
+        public static String WechatUrl => AppConfigurtaion.Configuration["WechatUrl"];
+        public static String WechatAppId => AppConfigurtaion.Configuration["WechatAppId"];
+        public static String WechatAppSecret => AppConfigurtaion.Configuration["WechatAppSecret"];
+        public static String EventAppId => AppConfigurtaion.Configuration["EventAppId"];
+        public static String EventAppSecret => AppConfigurtaion.Configuration["EventAppSecret"];
+        public static String PlatformEventAppId => AppConfigurtaion.Configuration["PlatformEventAppId"];
+        public static String PlatformEventAppSecret => AppConfigurtaion.Configuration["PlatformEventAppSecret"];
+        public static String FileUrl => AppConfigurtaion.Configuration["FileUrl"];
+        public static String FileAppId => AppConfigurtaion.Configuration["FileAppId"];
+        public static String FileAppSecret => AppConfigurtaion.Configuration["FileAppSecret"];
+        public static String ReverseGeocodingUrl => AppConfigurtaion.Configuration["ReverseGeocodingUrl"];
+        public static String MapAppId => AppConfigurtaion.Configuration["MapAppId"];
+        public static String MapAppSecret => AppConfigurtaion.Configuration["MapAppSecret"];
+        public static String AuthorizeAppId => AppConfigurtaion.Configuration["AuthorizeAppId"];
+        public static String AuthorizeAppSecret => AppConfigurtaion.Configuration["AuthorizeAppSecret"];
+        public static String AuthorizeUrl => AppConfigurtaion.Configuration["AuthorizeUrl"];
     }
 }
